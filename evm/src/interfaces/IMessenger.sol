cd082923378f4d942015a0f19b16e2852e317067
Jordan Hall <oighty6@gmail.com>
2025-11-06 16:03:18 -0600

    chore: prettier

--- evm/src/interfaces/IMessenger.sol
+++ evm/src/interfaces/IMessenger.sol
@@ -4,5 +4,9 @@ pragma solidity ^0.8;
 import { IOrderBook } from "./IOrderBook.sol";
 
 interface IMessenger {
-    function sendFillReport(uint32 destinationChainId, IOrderBook.FillReport calldata report, bytes calldata messageData) external;
+    function sendFillReport(
+        uint32 destinationChainId,
+        IOrderBook.FillReport calldata report,
+        bytes calldata messageData
+    ) external;
 }
