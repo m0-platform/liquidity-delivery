760e764ce9de35b08dd79684c1c57f322be8cbfd
Jordan Hall <oighty6@gmail.com>
2025-12-29 11:45:30 -0600

    chore: update license

--- evm/src/OrderBook.sol
+++ evm/src/OrderBook.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.26;
 
 import { IERC20 } from "../lib/common/lib/openzeppelin-contracts-upgradeable/lib/openzeppelin-contracts/contracts/interfaces/IERC20.sol";
