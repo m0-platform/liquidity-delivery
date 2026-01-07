997851bcb3b376a44896798dbd1ea7839ab62fb4
denalimarsh <denalimarsh@gmail.com>
2026-01-07 15:22:36 +0100

    evm: add tests

--- evm/test/unit/OrderBook/pause.t.sol
+++ evm/test/unit/OrderBook/pause.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.26;
+pragma solidity 0.8.33;
 
 import { OrderBookTestBase } from "./OrderBookTestBase.t.sol";
 import { IAccessControl } from "../../../lib/common/lib/openzeppelin-contracts-upgradeable/lib/openzeppelin-contracts/contracts/access/IAccessControl.sol";
